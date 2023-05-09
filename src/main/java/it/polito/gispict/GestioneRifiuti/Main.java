d21c3fac51b76122560dabb44cb010b39d626228
gabrielecarru <gabrielecarru@mbp-di-gabriele.wind3.hub>
2023-05-09 17:04:27 +0200

    Aggiunto commento di prova inutile

--- src/main/java/it/polito/gispict/GestioneRifiuti/Main.java
+++ src/main/java/it/polito/gispict/GestioneRifiuti/Main.java
@@ -3,5 +3,7 @@ package it.polito.gispict.GestioneRifiuti;
 public class Main {
 	public static void main(String[] args) {
         EntryPoint.main(args);
+        
+        // Aggiunto commento di prova inutile
     }
 }
