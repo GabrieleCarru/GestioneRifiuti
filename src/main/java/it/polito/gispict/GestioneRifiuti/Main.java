ffce7677a1764e4c12e88b6630488b01add674cd
gabrielecarru <gabrielecarru@mbp-di-gabriele.station>
2023-05-12 17:50:47 +0200

    Riprestino versione precedente al doppio commit

--- src/main/java/it/polito/gispict/GestioneRifiuti/Main.java
+++ src/main/java/it/polito/gispict/GestioneRifiuti/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
         EntryPoint.main(args);
         
-        // La speranza è quella di aver risolto definitivamente!
+        // Riprestino versione precedente; problema con commit (chiede creazione branch)
+        
     }
 }
