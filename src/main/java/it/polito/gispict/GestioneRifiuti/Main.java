0b3df0d181d7a330d8184cf8091245f725008767
franc <franc@LAPTOP-PMLK46VR>
2023-05-12 12:01:34 +0200

    Commit prova fra

--- src/main/java/it/polito/gispict/GestioneRifiuti/Main.java
+++ src/main/java/it/polito/gispict/GestioneRifiuti/Main.java
@@ -4,6 +4,6 @@ public class Main {
 	public static void main(String[] args) {
         EntryPoint.main(args);
         
-        // La speranza è quella di aver risolto definitivamente!
+        // La speranza è quella di aver risolto di nuovo!
     }
 }
