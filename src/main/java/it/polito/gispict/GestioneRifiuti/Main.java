ebefbb46994f105c3bef847b8fd349c06ffc9991
gabrielecarru <gabrielecarru@mbp-di-gabriele.wind3.hub>
2023-05-12 12:03:40 +0200

    Commit prova Gab 2

--- src/main/java/it/polito/gispict/GestioneRifiuti/Main.java
+++ src/main/java/it/polito/gispict/GestioneRifiuti/Main.java
@@ -4,6 +4,6 @@ public class Main {
 	public static void main(String[] args) {
         EntryPoint.main(args);
         
-        // La speranza è quella di aver risolto di nuovo!
+        // Cambini <3
     }
 }
