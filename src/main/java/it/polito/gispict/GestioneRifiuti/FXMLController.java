4959bc3fc511c84f5792f05f7f9656a92e32a80d
gabrielecarru <gabrielecarru@mbp-di-gabriele.station>
2023-05-14 12:11:02 +0200

    Interfaccia grafica funzionante (definiti btn e txt)

--- src/main/java/it/polito/gispict/GestioneRifiuti/FXMLController.java
+++ src/main/java/it/polito/gispict/GestioneRifiuti/FXMLController.java
@@ -4,22 +4,74 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
-public class FXMLController implements Initializable {
-    
+public class FXMLController {
+
     @FXML
-    private Label label;
-    
+    private ResourceBundle resources;
+
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private URL location;
+
+    @FXML
+    private Button btnAcquisisciSensore;
+
+    @FXML
+    private Button btnAnnulla;
+
+    @FXML
+    private Button btnAvviaSimulazione;
+
+    @FXML
+    private Button btnElimina;
+
+    @FXML
+    private Button btnInformazioni;
+
+    @FXML
+    private TextField txtNumCestini;
+
+    @FXML
+    private TextArea txtRisultato;
+
+    @FXML
+    void acquisireSensore(ActionEvent event) {
+    	System.out.println("Funziona il pulsante");
     }
-    
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+
+    @FXML
+    void annullaSimulazione(ActionEvent event) {
+
+    }
+
+    @FXML
+    void avvioSimulazione(ActionEvent event) {
+
+    }
+
+    @FXML
+    void eliminaRisultato(ActionEvent event) {
+
+    }
+
+    @FXML
+    void fornisciInformazioni(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert btnAcquisisciSensore != null : "fx:id=\"btnAcquisisciSensore\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnAnnulla != null : "fx:id=\"btnAnnulla\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnAvviaSimulazione != null : "fx:id=\"btnAvviaSimulazione\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnElimina != null : "fx:id=\"btnElimina\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnInformazioni != null : "fx:id=\"btnInformazioni\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtNumCestini != null : "fx:id=\"txtNumCestini\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtRisultato != null : "fx:id=\"txtRisultato\" was not injected: check your FXML file 'Scene.fxml'.";
+
+    }
+
 }
